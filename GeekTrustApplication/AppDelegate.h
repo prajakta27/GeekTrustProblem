8bee50683730f7c54db4ecfcc42083af4af96b35
prajakta sonawane <prajakta.sonawane@porteamedical.com>
2017-01-17 19:27:05 +0530

    Initial Commit

---
+++ GeekTrustApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GeekTrustApplication
+//
+//  Created by Prajakta Vishwas Sonawane on 1/17/17.
+//  Copyright © 2017 Prajakta Vishwas Sonawane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GeekTrustApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GeekTrustApplication
+//
+//  Created by Prajakta Vishwas Sonawane on 1/17/17.
+//  Copyright © 2017 Prajakta Vishwas Sonawane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
